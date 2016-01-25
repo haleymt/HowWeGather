e42d03ccdf57462d44ad2a93ab44c17716c94f15
Haley <haleymthurston@gmail.com>
2016-01-25 15:26:59 -0800

    first commit

---
+++ angie.js
@@ -0,0 +1,66 @@
+function Viewer($main) {
+  this.$doc = $main;
+  this.registerEvents();
+}
+
+// $ is a jQuery namespace
+$.extend(Viewer.prototype, {
+
+  registerEvents: function () {
+    $('#panel1').on('click', function() {
+      $('#pdf1').css('display', 'block');
+    });
+
+    $('#panel2').on('click', function() {
+      $('#pdf2').css('display', 'block');
+    });
+
+    $('#pdf2').on('click', function() {
+      $('#pdf2').css('display', 'none');
+    });
+
+    $('#pdf1').on('click', function() {
+      $('#pdf1').css('display', 'none');
+    });
+
+    $('#overlay1').on('click', function(e) {
+      e.stopPropagation();
+      downloadFile("pdfs/how-we-gather1.pdf");
+    });
+
+    $('#overlay2').on('click', function(e) {
+      e.stopPropagation();
+      downloadFile("pdfs/how-we-gather2.pdf");
+    });
+
+    $('.download-button').click(function(e) {
+      // e.preventDefault();
+      // downloadFile();
+    });
+  },
+});
+
+function downloadFile(file) {
+  var isChrome = navigator.userAgent.toLowerCase().indexOf('chrome') > -1;
+  var isFirefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
+  var isSafari = navigator.userAgent.toLowerCase().indexOf('safari') > -1;
+  var downloadUrl = file;
+
+  if (isChrome || isFirefox) {
+    var link = document.createElement('a');
+    link.download = "how_we_gather";
+    link.href = downloadUrl;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  } else if (isSafari) {
+    window.location.assign(downloadUrl);
+  } else {
+    window.open(downloadUrl, 'Download');
+  }
+}
+
+$(function () {
+  var $main = $('#main');
+  v = new Viewer($main);
+});
